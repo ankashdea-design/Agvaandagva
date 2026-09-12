3d7fa8c29523d6e9e235ab2ef14be60823cb4a77
ankashdea-design <ankashdea@gmail.com>
2026-09-12 23:43:15 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
-
+export const dynamic = "force-dynamic";
 export const metadata: Metadata = {
   title: "KinderCare MN",
   description: "Цэцэрлэгийн өдөр тутмын тайлан систем",
